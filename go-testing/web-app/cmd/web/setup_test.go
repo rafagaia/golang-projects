3b4d3f7067f3f225ad1fd19d1f600de3a227491f
rafagaia <rafaelvsgaia@gmail.com>
2023-03-22 15:49:08 -0300

    test: implements db connection in setup_test TestMain function similar to how its done in main.go

--- go-testing/web-app/cmd/web/setup_test.go
+++ go-testing/web-app/cmd/web/setup_test.go
@@ -3,8 +3,10 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
+	"webapp/pkg/db"
 )
 
 // now app variable exists in entire scope of tests, so we don't need to declare in each _test.go
@@ -21,6 +23,17 @@ func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
 
 	app.Session = getSession()
+
+	app.DSN = "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5"
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// don't close until current function exits
+	defer conn.Close()
+
+	app.DB = db.PostgresConn{DB: conn}
 
 	os.Exit(m.Run())
 }
