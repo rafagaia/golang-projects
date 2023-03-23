ca31e3961c11829a9fdb08b1e1b2dcc291b89f42
rafagaia <rafaelvsgaia@gmail.com>
2023-03-22 21:03:02 -0300

    feat: using repository pattern, implements DatabaseRepo Interface in pkg/repository/repository.go

---
+++ go-testing/web-app/pkg/repository/repository.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"database/sql"
+	"webapp/pkg/data"
+)
+
+// anything that we'll use with Repository Pattern must satisfy this interface:
+type DatabaseRepo interface {
+	Connection() *sql.DB
+	AllUsers() ([]*data.User, error)
+	GetUser(id int) (*data.User, error)
+	GetUserByEmail(email string) (*data.User, error)
+	UpdateUser(u data.User) error
+	DeleteUser(id int) error
+	InsertUser(user data.User) (int, error)
+	ResetPassword(id int, password string) error
+	InsertUserImage(i data.UserImage) (int, error)
+}
