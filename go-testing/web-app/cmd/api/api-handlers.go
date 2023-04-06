7f1c4dc57d46a5a243cef99e62d9a1b9627edf3d
rafagaia <rafaelvsgaia@gmail.com>
2023-04-06 05:01:51 -0300

    feat: implements authenticate handler function in api-handlers.go

--- go-testing/web-app/cmd/api/api-handlers.go
+++ go-testing/web-app/cmd/api/api-handlers.go
@@ -1,6 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Credentials struct {
+	Username string `json:"email"`
+	Password string `json:"password"`
+}
 
 /*
 *	1. takes a JSON payload from client, with username and password
@@ -8,15 +18,32 @@ import "net/http"
 *	3. if match found, generates and returns JSON with JWT Token
 **/
 func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
 	// read a JSON payload
-
+	err := app.readJSON(w, r, &creds)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 	// look up the user by email address
-
+	user, err := app.DB.GetUserByEmail(creds.Username)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 	// check password
-
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(creds.Password))
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 	// generate tokens (jwt and refresh)
-
+	tokenPairs, err := app.generateTokenPair(user)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+	}
 	// send tokens to user
+	_ = app.writeJSON(w, http.StatusOK, tokenPairs)
 }
 
 // takes JSON from client with a valid JWT Token, generates a new one, and returns it back
