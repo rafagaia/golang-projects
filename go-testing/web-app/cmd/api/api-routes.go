0008f1cc3d5cbac30d90ff7cfcf05a790e7d703c
rafagaia <rafaelvsgaia@gmail.com>
2023-04-07 04:35:59 -0300

    fix: rename routes.go to api-routes.go; registers middlewares authRequired and enableCors to routes handler.

--- go-testing/web-app/cmd/api/api-routes.go
+++ go-testing/web-app/cmd/api/api-routes.go
@@ -12,7 +12,7 @@ func (app *application) routes() http.Handler {
 
 	// register middleware
 	mux.Use(middleware.Recoverer)
-	// mux.Use(app.enableCORS)
+	mux.Use(app.enableCORS)
 
 	/*
 	* authentication routes:
@@ -22,7 +22,7 @@ func (app *application) routes() http.Handler {
 	mux.Post("/auth", app.authenticate)
 	mux.Post("/refresh-token", app.refresh)
 
-	// test handler - for development
+	/* test handler - for development
 	mux.Get("/test", func(w http.ResponseWriter, r *http.Request) {
 		var payload = struct {
 			Message string `json:"message"`
@@ -30,13 +30,15 @@ func (app *application) routes() http.Handler {
 			Message: "Greetings, humans!",
 		}
 		_ = app.writeJSON(w, http.StatusOK, payload)
-	})
+	})*/
 
 	// protected routes - must have a valid jwt token to access
 
 	// all nested routes here have "/users" to route url prepended
 	mux.Route("/users", func(mux chi.Router) {
 		// use auth middleware
+		mux.Use(app.authRequired)
+
 		mux.Get("/", app.allUsers)
 		mux.Get("/{userID}", app.getUser)
 		mux.Delete("/{userID}", app.deleteUser)
